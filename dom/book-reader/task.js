aef36c3d114e433c2ba14115dd6a29730ad8c729
Сергей <vaskovsergey95@gmail.com>
2023-03-21 23:41:48 +0500

    Решение 1.1

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,17 +1,22 @@
-
-const book =document.querySelector('.book');
+const book = document.querySelector('.book');
 const font = document.querySelectorAll('.font-size')
 
-font.forEach(item =>{
-    item.addEventListener('click', () =>{
-        font.forEach(el=>{ el.classList.remove('font-size_active'); });
+font.forEach(item => {
+    item.addEventListener('click', (event) => {
+        font.forEach(el => {
+            el.classList.remove('font-size_active');
+        });
         item.classList.add('font-size_active')
+        event.preventDefault();
 
-        if(item.classList.contains('font-size_big')){
+        if (item.classList.contains('font-size_big')) {
             book.classList.add('book_fs-big')
-        }
-        if(item.classList.contains('font-size_small')){
+        } else if (item.classList.contains('font-size_small')) {
             book.classList.add('book_fs-small')
+            book.classList.remove('book_fs-big')
+        } else {
+            book.classList.remove('book_fs-small')
+            book.classList.remove('book_fs-big')
         }
     })
 
