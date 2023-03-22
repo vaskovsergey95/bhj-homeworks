63432c2e808fb1a2eaac8702bc8a226709b17ec5
Сергей <vaskovsergey95@gmail.com>
2023-03-22 22:10:14 +0500

    Решение 1.1

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,24 +1,12 @@
 const rev = Array.from(document.querySelectorAll('.reveal'));
-let res;
 
 function isVisible(e) {
     const {top, bottom} = e.getBoundingClientRect();
-    if (bottom < 0) {
-        res = false;
-    } else if (top > window.innerHeight) {
-        res = false;
-    }
-    res = true;
+    return (!(bottom < 0 && top > window.innerHeight));
 }
 
-for (let i = 0; i < rev.length; i++) {
-    isVisible(rev[i])
+rev.forEach(elem => {
     window.addEventListener('scroll', () => {
-        if (res === true) {
-            rev[i].classList.toggle('reveal_active')
-        }
-
+        isVisible(elem) ? elem.classList.toggle('reveal_active') : false;
     })
-}
-
-
+})
