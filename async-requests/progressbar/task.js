93fe0502d1f6ca90564f8b3a8eabb42afe435e27
Сергей <vaskovsergey95@gmail.com>
2023-05-03 22:23:57 +0500

    3 версия

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -2,14 +2,14 @@ let xhr = new XMLHttpRequest();
 const progress = document.getElementById("progress");
 const form = document.getElementById("form");
 
-form.addEventListener('submit', () => {
+form.addEventListener('submit', e => {
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
     xhr.upload.addEventListener('progress', elem => {
         progress.value = elem.loaded / elem.total;
-
+    })
     const formData = new FormData(form);
     xhr.send(formData);
 
-    elem.preventDefault();
-    })
+    e.preventDefault();
+
 })
